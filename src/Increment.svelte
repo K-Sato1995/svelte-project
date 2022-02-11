1f0c35269dd213dd5e212bf40c9de61d205394ab
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-11 13:23:26 +0900

    Add reactivity

--- src/Increment.svelte
+++ src/Increment.svelte
@@ -3,6 +3,8 @@
   const increment = () => {
     count += 1;
   };
+  // Gets triggered whenever the count value gets updated.
+  $: console.log("the count is " + count);
 </script>
 
 <main><button on:click={increment}>Clicked {count} times</button></main>
